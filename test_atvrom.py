fc6ef7525e4f1c930d44c949f69d95c4f14131fd
MihaiStoian-arch <mihaistoian889@gmail.com>
2025-12-02 17:20:13 +0200

    Update test_atvrom.py

--- test_atvrom.py
+++ test_atvrom.py
@@ -1,6 +1,7 @@
+# test_atvrom.py
 from monitor.sites.atvrom import scrape_atvrom
 
-URL = "https://atvrom.ro/motociclete/cfmoto-125-nk-abs-25"
+URL = "https://www.atvrom.ro/motociclete/cfmoto-125-nk-abs-25"
 
 price = scrape_atvrom(URL)
 print("Preț găsit:", price)
