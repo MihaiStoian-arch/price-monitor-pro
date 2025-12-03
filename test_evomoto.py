4ee83174d8a0ef9ffdb69c03297a9287e496d2a7
MihaiStoian-arch <mihaistoian889@gmail.com>
2025-12-03 18:51:46 +0200

    Update test_evomoto.py

--- test_evomoto.py
+++ test_evomoto.py
@@ -1,6 +1,3 @@
 from monitor.sites.evo_moto import scrape_evomoto
 
-url = "https://evo-moto.ro/kawasaki-vulcan-s-2026"
-
-price = scrape_evomoto(url)
-print("Preț EVOMOTO:", price)
+print("Preț EVO MOTO:", scrape_evomoto("https://evo-moto.ro/kawasaki-vulcan-s-2026"))
