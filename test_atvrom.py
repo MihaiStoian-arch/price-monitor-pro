9875601b38612f8aef2ae76e72a0260946b868d9
MihaiStoian-arch <mihaistoian889@gmail.com>
2025-12-04 18:11:20 +0200

    Delete test_atvrom.py

--- test_atvrom.py
+++
@@ -1,7 +0,0 @@
-# test_atvrom.py
-from monitor.sites.atvrom import scrape_atvrom
-
-URL = "https://www.atvrom.ro/motociclete/cfmoto-125-nk-abs-25"
-
-price = scrape_atvrom(URL)
-print("Preț găsit:", price)
